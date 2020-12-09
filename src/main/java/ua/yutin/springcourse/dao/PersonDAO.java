fdc9cc3ddf75ee02fdec6f760b1c6cb521c2756a
IhorYutinDev <ihor.yutin.dev@gmail.com>
2020-12-09 11:29:24 +0200

    first commit

---
+++ src/main/java/ua/yutin/springcourse/dao/PersonDAO.java
@@ -0,0 +1,34 @@
+package ua.yutin.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ua.yutin.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Neo"));
+        people.add(new Person(++PEOPLE_COUNT, "Ihoryok"));
+        people.add(new Person(++PEOPLE_COUNT, "Glebushka"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(final int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
